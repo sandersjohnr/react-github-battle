f9b99307fba194d27a587e9bff9cc1bf32c8ca45
John Sanders <john.sanders@cittacorp.com>
2017-05-15 12:17:18 -0400

    initialize webpack config

---
+++ app/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// require('./index.css');
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello world!
+      </div>
+    );
+  }
+}
+
+// export default App;
+const $app = document.getElementById('app');
+
+ReactDOM.render(
+  <App/>,
+  $app
+);
